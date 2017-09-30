6e8eb44f6326ffceddc4d99914747fd0716d9af0
djangojazz <brettmorin2@gmail.com>
2017-09-30 07:19:19 -0700

    Added transactions as it's own javascript file

---
+++ AspNetMVCCheckRegister/Scripts/transactions.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+  var balance = $('#transactionsTable tr td').last().html();
+  var footer = $("#transactionsSummary");
+
+  footer.html('Your current Balance is <strong>' + balance + '</strong>');
+  if (balance > 0) {
+    footer.css('color', 'green')
+  }
+  else {
+    footer.css('color', 'red')
+  }
+
+});
